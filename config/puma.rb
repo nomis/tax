80bf47c11becc084b37bbd513febcbafc449de88
Simon Arlott <sa.me.uk>
2021-12-04 11:44:03 +0000

    configure db, listening IP

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-set_default_host "127.80.0.2"
+set_default_host ENV.fetch("PUMA_HOST") { "127.80.0.2" }
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
